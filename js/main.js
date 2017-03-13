7271eb82d2a0e4193c54fadbd8bc59bc4aaa6876
Doug Modie <dmodie@gmail.com>
2017-03-13 14:32:41 -0700

    first commit, works but buggy

---
+++ js/main.js
@@ -0,0 +1,78 @@
+
+(function localFileVideoPlayer() {
+ var $canvas = $('#canvas')
+	function captureFrame(){
+    	var video = document.getElementById('video')
+      console.log ("video")
+      console.log (video)
+      console.log ("width")
+      console.log (video.videoWidth)
+      console.log ("height")
+      console.log (video.videoHeight)
+      var $canvas = $('<canvas></canvas>');
+      $canvas.css("width", video.videoWidth)
+      var $canvasContainer = $('#canvas-container');
+      $canvasContainer.empty();
+      $canvasContainer.append($canvas);
+//			$canvas[0].getContext('2d').drawImage(video, 0, 0,video.videoWidth, video.videoHeight);
+			$canvas[0].getContext('2d').drawImage(video, 0, 0,video.videoWidth, video.videoHeight,0,0,video.videoWidth, video.videoHeight);
+  
+  
+  
+  }
+	$("#captureButton").on('click', captureFrame);
+	function uploadVideoFile (event) {
+      function dataUploaded(){
+        console.log ('data uploaded')
+        console.log (fr.result)
+      }
+      var file = this.files[0]
+      var fr = new FileReader();
+      fr.onloadend = dataUploaded;
+      //fr.readAsText(file);
+      fr.readAsDataURL(file);
+  }
+  
+  
+  function playSelectedFile (event) {
+      var URL = window.URL || window.webkitURL
+      var displayMessage = function (message, isError) {
+        var element = document.querySelector('#message')
+        element.innerHTML = message
+        element.className = isError ? 'error' : 'info'
+      }
+
+      var file = this.files[0]
+      var type = file.type
+      var videoNode = document.querySelector('video')
+      var canPlay = videoNode.canPlayType(type)
+      if (canPlay === '') canPlay = 'no'
+      var message = 'Can play type "' + type + '": ' + canPlay
+      var isError = canPlay === 'no'
+      //displayMessage(message, isError)
+
+      if (isError) {
+        return
+      }
+      var fileURL = URL.createObjectURL(file)
+      videoNode.src = fileURL
+  
+  }  
+  
+  var $inputNode = $('.video-input')
+  $inputNode[0].addEventListener('change', playSelectedFile, false)
+
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
